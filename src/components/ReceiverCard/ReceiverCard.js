8cea8e2fcacbf1d163512db8b4a48b02ba975378
Patrick Drejerczak <drejerczak@aol.com>
2021-08-09 08:38:28 +0200

    card update

--- src/components/ReceiverCard/ReceiverCard.js
+++ src/components/ReceiverCard/ReceiverCard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./ReceiverCard.css";
-import { Card } from "react-bootstrap";
 
 const ReceiverCard = ({
   imgURL,
@@ -11,18 +10,39 @@ const ReceiverCard = ({
   details,
 }) => {
   return (
-    <Card>
-      <Card.Img variant="top" src={imgURL} />
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{need}</Card.Subtitle>
-        <Card.Text>
-          <p>Location: {location}</p>
-          <p>Requesting for: {requestingFor}</p>
-          <p className="small">Details: {details}</p>
-        </Card.Text>
-      </Card.Body>
-    </Card>
+    <div class="row gx-5">
+      <div class="col-lg-4 mb-5 h-100">
+        <div class="card h-100 shadow border-0">
+          <img
+            class="card-img-top"
+            src={imgURL}
+            alt="..."
+            height="300px"
+            width="250px"
+          />
+          <div class="card-body p-4 h-100">
+            <div class="badge bg-primary bg-gradient rounded-pill mb-2 align-items">
+              {location}
+            </div>
+            <a class="text-decoration-none link-dark stretched-link" href="#!">
+              <div class="h5 card-title mb-3">{name}</div>
+            </a>
+            <div class="h6 card-title mb-3">Requests: {need}VND</div>
+            <p class="card-text mb-0">{details}</p>
+          </div>
+          <div class="card-footer p-4 pt-0 bg-transparent border-top-0">
+            <div class="d-flex align-items-end justify-content-between">
+              <div class="d-flex align-items-center">
+                <div class="small">
+                  <div class="fw-bold">Needed for: {requestingFor}</div>
+                  <div class="text-muted">Date posted: March 12, 2021</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
